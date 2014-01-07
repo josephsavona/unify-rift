ef91e4acd3d3686bd9b826b65de29a8ecd867742
Joseph Savona <joesavona@gmail.com>
2014-01-06 20:09:48 -0800

    add basic tests, fix getBase() reference

---
+++ test/test.rift.js
@@ -0,0 +1,9 @@
+var search = module.exports = {
+  search: {
+    help: 'HELP STRING',
+    // path relative to API root
+    url: '/rel/path',
+    // the http method type: get, post, put, del
+    method: 'get'
+  }
+};
