c63620d00ea4495ddf8fdab5c8fa2e63fdbfe93b
Steve Davie <steve@digitalskunk.com>
2014-04-25 11:24:36 -0700

    removing unused libs from spec

--- test/socket_spec.js
+++ test/socket_spec.js
@@ -7,11 +7,7 @@ var test                         = require('tap').test;
        client                    = require('./test.rift'),
        nock                     = require('nock'),
        sinon                    = require('sinon'),
-       Promise                = require('bluebird'),
        server                   = require('./test_socket_server');
-
-// record http traffic
-// nock.recorder.rec();
 
 var api = rift();
 api.set('base', '/api');
@@ -21,7 +17,6 @@ api.registerResolver(RiftResolver);
 
 test('should make a request using socket', function (t) {
   server.start();
-
   api.request('socketTest').then(function (response) {
     t.equals(response.data.message, 'socketTest has been received.');
     t.end();
